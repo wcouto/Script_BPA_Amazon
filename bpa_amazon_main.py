2078f8b8d9a59201ed3d0761f92db86b889e4808
Willian Couto <willian_994@hotmail.com>
2021-08-04 00:04:01 -0300

    Correction on Path

--- bpa_amazon_main.py
+++ bpa_amazon_main.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import xlsxwriter
+import os
 
-
+def get_root_dir():
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    return root_dir
 
 def get_url(driver_path, url):
     driver = webdriver.Chrome(driver_path)
@@ -42,20 +45,23 @@ def populate_dicts(products, list_products, list_prices):
             except:
                 product_price = product_price_whole + '00'
         except:
-            product_price = "Dado indisponível"
+            product_price = "Unavailable Data"
 
         list_products.append(product_name)
         list_prices.append(product_price)
 
-def create_excel(list_products, list_prices):
-    df = pd.DataFrame({'Nome do produto': list_products, 'Preço do produto': list_prices})
-    writer = pd.ExcelWriter("Pesquisa_Iphone_Amazon_Pagina1.xlsx", engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Pesquisa_Iphone_Amazon_Pagina1', index=False)
+def create_excel(list_products, list_prices, root_dir):
+    df = pd.DataFrame({'Product Name': list_products, 'Product Price': list_prices})
+    writer = pd.ExcelWriter(root_dir + "\\Output\\Search_Iphone_Amazon_Page1.xlsx", engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Search_Iphone_Amazon_Page1', index=False)
     writer.save()
 
 def main():
+
+    root_dir = get_root_dir()
+
     print("Receiving parameters to build the driver.")
-    driver = get_url("C:\\Users\willi\\Projeto Python\\Script_BPA_Amazon\\Chrome_Driver\\chromedriver.exe", 'https://www.amazon.com.br/')
+    driver = get_url(root_dir + "\\Chrome_Driver\\chromedriver.exe", 'https://www.amazon.com.br/')
     wait(driver)
 
     print("Searching 'Iphone' on amazon website.")
@@ -73,7 +79,7 @@ def main():
     populate_dicts(products, list_products, list_prices)
 
     print("Creating Excel.")
-    create_excel(list_products, list_prices)
+    create_excel(list_products, list_prices, root_dir)
 
 if __name__ == '__main__':
     main()
