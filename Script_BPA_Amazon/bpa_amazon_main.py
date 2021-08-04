b22a439ad8db44a45f0f1379371800f390a4536c
Willian Couto <willian_994@hotmail.com>
2021-08-03 23:36:54 -0300

    Last implementation

--- Script_BPA_Amazon/bpa_amazon_main.py
+++ Script_BPA_Amazon/bpa_amazon_main.py
@@ -3,44 +3,77 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import xlsxwriter
 
-driver_path = "C:\\Users\willi\\Projeto Python\\Script_BPA_Amazon\\Chrome_Driver\\chromedriver.exe"
 
-driver = webdriver.Chrome(driver_path)
-driver.get('https://www.amazon.com.br/')
 
-search_box = driver.find_element_by_id('twotabsearchtextbox').send_keys("iphone")
+def get_url(driver_path, url):
+    driver = webdriver.Chrome(driver_path)
+    driver.get(url)
+    return driver
 
-search_button = driver.find_element_by_id("nav-search-submit-text").click()
+def wait(driver):
+    driver.implicitly_wait(5)
 
-driver.implicitly_wait(5)
+def do_search(driver):
+    search_box = driver.find_element_by_id('twotabsearchtextbox').send_keys("iphone")
+    search_button = driver.find_element_by_id("nav-search-submit-text").click()
 
-soup = BeautifulSoup(driver.page_source, "lxml")
+def get_all_products(driver):
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    products = soup.find_all('div', {'data-asin': True, 'data-component-type': 's-search-result'})
+    return products
 
-driver.implicitly_wait(5)
+def create_list_products():
+    list_products = []
+    return list_products
 
-products = soup.find_all('div', {'data-asin': True, 'data-component-type': 's-search-result'})
+def create_list_prices():
+    list_prices = []
+    return list_prices
 
-driver.implicitly_wait(5)
+def populate_dicts(products, list_products, list_prices):
 
-list_products = []
-list_prices = []
-
-for product in products:
-    product_name = product.h2.text
-    try:
-        product_price_whole = product.find('span', class_= 'a-price-whole').text
+    for product in products:
+        product_name = product.h2.text
         try:
-            product_price_fration = product.find('span', class_= 'a-price-fraction').text
-            product_price = product_price_whole + product_price_fration
+            product_price_whole = product.find('span', class_= 'a-price-whole').text
+            try:
+                product_price_fration = product.find('span', class_= 'a-price-fraction').text
+                product_price = product_price_whole + product_price_fration
+            except:
+                product_price = product_price_whole + '00'
         except:
-            product_price = product_price_whole + '00'
-    except:
-        product_price = "Dado indisponível"
+            product_price = "Dado indisponível"
 
-    list_products.append(product_name)
-    list_prices.append(product_price)
+        list_products.append(product_name)
+        list_prices.append(product_price)
 
-df = pd.DataFrame({'Nome do produto': list_products, 'Preço do produto': list_prices})
-writer = pd.ExcelWriter("Pesquisa_Iphone_Amazon_Pagina1.xlsx", engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Pesquisa_Iphone_Amazon_Pagina1', index=False)
-writer.save()
+def create_excel(list_products, list_prices):
+    df = pd.DataFrame({'Nome do produto': list_products, 'Preço do produto': list_prices})
+    writer = pd.ExcelWriter("Pesquisa_Iphone_Amazon_Pagina1.xlsx", engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Pesquisa_Iphone_Amazon_Pagina1', index=False)
+    writer.save()
+
+def main():
+    print("Receiving parameters to build the driver.")
+    driver = get_url("C:\\Users\willi\\Projeto Python\\Script_BPA_Amazon\\Chrome_Driver\\chromedriver.exe", 'https://www.amazon.com.br/')
+    wait(driver)
+
+    print("Searching 'Iphone' on amazon website.")
+    do_search(driver)
+    wait(driver)
+
+    print("Receiving all products from the first page.")
+    products = get_all_products(driver)
+    wait(driver)
+
+    list_products = create_list_products()
+    list_prices = create_list_prices()
+
+    print("Saving the products informations")
+    populate_dicts(products, list_products, list_prices)
+
+    print("Creating Excel.")
+    create_excel(list_products, list_prices)
+
+if __name__ == '__main__':
+    main()
